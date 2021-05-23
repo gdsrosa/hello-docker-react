1efb0c72a02350f2c9091acfb1de076b24631f15
Gabriel Rosa <gabrieldsrosa@gmail.com>
2021-05-23 00:12:05 -0300

    feat: update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders application title', () => {
-  const { getByText } = render(<App />);
-  expect(getByText('React + TS + Docker')).toBeInTheDocument();
+describe('app renders', () => {
+  it('renders application title', () => {
+    const { getByText } = render(<App />);
+    expect(getByText('React + TS + Docker')).toBeInTheDocument();
+  });
 });
