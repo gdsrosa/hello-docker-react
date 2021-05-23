c024d63a7533abc04e32390ae83efb802d65f407
Gabriel Rosa <gabrieldsrosa@gmail.com>
2021-05-23 00:11:46 -0300

    feat: abstract to function

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,22 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+
+  function handleCounterIncrement() {
+    setCount(count + 1);
+  }
+  function handleCounterDecrement() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+  function handleCounterDoubleIncrement() {
+    setCount(count + 2);
+  }
+  function handleCounterReset() {
+    setCount(0);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,23 +28,16 @@ function App() {
         <p>React + TS + Docker</p>
         <p>Count: {count}</p>
         <div className="control">
-          <button type="button" onClick={() => setCount(count + 1)}>
+          <button type="button" onClick={handleCounterIncrement}>
             Increment
           </button>
-          <button
-            type="button"
-            onClick={() => {
-              if (count > 0) {
-                setCount(count - 1);
-              }
-            }}
-          >
+          <button type="button" onClick={handleCounterDecrement}>
             Decrement
           </button>
-          <button type="button" onClick={() => setCount(count + 2)}>
+          <button type="button" onClick={handleCounterDoubleIncrement}>
             Double Increment
           </button>
-          <button type="button" onClick={() => setCount(0)}>
+          <button type="button" onClick={handleCounterReset}>
             Reset Counter
           </button>
         </div>
